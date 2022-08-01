af9bea9d4bee0c778de48c556daa6b3f5563cc45
Heitor <heitor.cardoso39@gmail.com>
2022-08-01 06:02:28 -0300

    fix: alteração no userData para os meus dados

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
-};
+	nameUser: 'Heitor Moreira',
+	githubUser: 'ekheinn',
+	linkedinUser: 'heitor-moreira',
+	whatsappNumber: '+5561998592725',
+}
